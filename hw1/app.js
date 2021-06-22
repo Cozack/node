158e80f4615e169d555f7d5b115fd5e08f8ec42d
Cozack <dudycholeg@gmail.com>
2021-06-22 19:30:09 +0300

    test1

--- hw1/app.js
+++ hw1/app.js
@@ -6,13 +6,20 @@ const path = require('path');
 // fs.mkdir(`${__dirname}/1800/`,{recursive:true},(err) => {console.log(err)})
 // fs.mkdir(`${__dirname}/2000/`,{recursive:true},(err) => {console.log(err)})
 //
-// let firstUsersPath = path.join(__dirname,'1800','firstArrUsers.json');
+let firstUsersPath = path.join(__dirname,'1800','firstArrUsers.json');
 // let secondUsersPath = path.join(__dirname,'2000','secondArrUsers.json');
-// // fs.writeFile(firstUsersPath,firstArrUsers.toString(),(err) => {console.log(err)})
 
-let firstArrUsers = fs.readFileSync('./user1/package.json');
-let users1 = JSON.parse(firstArrUsers);
-console.log(users1)
+const firstArrUsers = [
+    {name:'Oleg',gender:'male'},
+    {name:'Oksana',gender:'female'},
+    {name:'Yulia',gender:'female'}
+
+]
+fs.writeFile(firstUsersPath,JSON.parse(firstArrUsers),(err) => {console.log(err)})
+
+// let firstArrUsers = fs.readFileSync('./user1/package.json');
+// let users1 = JSON.parse(firstArrUsers);
+// console.log(users1)
 
 
 
