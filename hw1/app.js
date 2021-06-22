9d7f33428d13531e27bffc281ad202001d3458d6
Cozack <dudycholeg@gmail.com>
2021-06-22 21:04:11 +0300

    hw 1 done

--- hw1/app.js
+++ hw1/app.js
@@ -1,32 +1,111 @@
 const fs = require('fs');
-require('./user1/package.json');
 const path = require('path');
 
-
-// fs.mkdir(`${__dirname}/1800/`,{recursive:true},(err) => {console.log(err)})
-// fs.mkdir(`${__dirname}/2000/`,{recursive:true},(err) => {console.log(err)})
+const path1800 = path.join(__dirname, '1800');
+const path2000 = path.join(__dirname, '2000');
+const male = path.join(__dirname, 'male');
+const female = path.join(__dirname, 'female');
+const allFiles = path.join(__dirname, 'allFiles');
 //
-let firstUsersPath = path.join(__dirname,'1800','firstArrUsers.json');
-// let secondUsersPath = path.join(__dirname,'2000','secondArrUsers.json');
-
-const firstArrUsers = [
-    {name:'Oleg',gender:'male'},
-    {name:'Oksana',gender:'female'},
-    {name:'Yulia',gender:'female'}
-
-]
-fs.writeFile(firstUsersPath,JSON.parse(firstArrUsers),(err) => {console.log(err)})
-
-// let firstArrUsers = fs.readFileSync('./user1/package.json');
-// let users1 = JSON.parse(firstArrUsers);
-// console.log(users1)
-
-
-
-// const firstArrUsers = [
-//     {name:'Oleg',gender:'male'},
-//     {name:'Oksana',gender:'female'},
-//     {name:'Yulia',gender:'female'}
+// fs.mkdir(path1800,{recursive:true},(err) => {console.log(err)})
+// fs.mkdir(allFiles,{recursive:true},(err) => {console.log(err)})
+// fs.mkdir(path2000,{recursive:true},(err) => {console.log(err)})
+// fs.mkdir(male, {recursive: true}, (err) => {
+//     console.log(err)
+// })
+// fs.mkdir(female, {recursive: true}, (err) => {
+//     console.log(err)
+// })
 //
-// ]
+// let firstUser = path.join(__dirname,'1800','user1.json');
+// let secondUser = path.join(__dirname,'1800','user2.json');
+// let thirdUser = path.join(__dirname,'1800','user3.json');
+//
+// fs.writeFile(firstUser,JSON.stringify({name:'Oleg',gender:'male'}),(err) => {console.log(err)})
+// fs.writeFile(secondUser,JSON.stringify({name:'Katya',gender:'female'}),(err) => {console.log(err)})
+// fs.writeFile(thirdUser,JSON.stringify({name:'Vitya',gender:'male'}),(err) => {console.log(err)})
+//
+// let fourthUser = path.join(__dirname,'2000','user4.json');
+// let fiveUser = path.join(__dirname,'2000','user5.json');
+// let sixUser = path.join(__dirname,'2000','user6.json');
+//
+//
+// fs.writeFile(fourthUser,JSON.stringify({name:'Inna',gender:'female'}),(err) => {console.log(err)})
+// fs.writeFile(fiveUser,JSON.stringify({name:'Olga',gender:'female'}),(err) => {console.log(err)})
+// fs.writeFile(sixUser,JSON.stringify({name:'Yura',gender:'male'}),(err) => {console.log(err)})
+
+
+
+
+// CHANGED FOLDERS BY PLACES
+
+// fs.readdir(path1800,(err, files) => {
+//     if (err){
+//         console.log(err)
+//         return
+//     }
+//     for (const file of files) {
+//         fs.rename(path.join(path1800,file),path.join(path2000,file),(err1) =>console.log(err))
+//
+//     }
+// })
+// fs.readdir(path2000, (err, files) => {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     for (const file of files) {
+//         fs.rename(path.join(path2000, file), path.join(path1800, file), (err1) => console.log(err))
+//
+//     }
+// })
+
+
+
+
+// SORT MALE AND FEMALE
+
+
+fs.readdir(path1800, (err, files) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    for (const file of files) {
+        fs.readFile(path.join(path1800, file), (err1, data) => {
+            if (!err1) {
+               const {gender} = JSON.parse(data.toString());
+               if (gender==='male'){
+                   fs.rename(path.join(path1800, file), path.join(male, file), (err1) => console.log(err))
+               } else {
+                   fs.rename(path.join(path1800, file), path.join(female, file), (err1) => console.log(err))
+
+               }
+            }
+        })
+
+    }
+})
+fs.readdir(path2000, (err, files) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    for (const file of files) {
+        fs.readFile(path.join(path2000, file), (err1, data) => {
+            if (!err1) {
+               const {gender} = JSON.parse(data.toString());
+               if (gender==='male'){
+                   fs.rename(path.join(path2000, file), path.join(male, file), (err1) => console.log(err))
+               } else {
+                   fs.rename(path.join(path2000, file), path.join(female, file), (err1) => console.log(err))
+
+               }
+            }
+        })
+
+    }
+})
+
+
 
