8fde30183204d4c8365a6ae2cdbb0f687d75fa75
Cozack <dudycholeg@gmail.com>
2021-06-22 19:27:02 +0300

    Initial commit

---
+++ hw1/app.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+require('./user1/package.json');
+const path = require('path');
+
+
+// fs.mkdir(`${__dirname}/1800/`,{recursive:true},(err) => {console.log(err)})
+// fs.mkdir(`${__dirname}/2000/`,{recursive:true},(err) => {console.log(err)})
+//
+// let firstUsersPath = path.join(__dirname,'1800','firstArrUsers.json');
+// let secondUsersPath = path.join(__dirname,'2000','secondArrUsers.json');
+// // fs.writeFile(firstUsersPath,firstArrUsers.toString(),(err) => {console.log(err)})
+
+let firstArrUsers = fs.readFileSync('./user1/package.json');
+let users1 = JSON.parse(firstArrUsers);
+console.log(users1)
+
+
+
+// const firstArrUsers = [
+//     {name:'Oleg',gender:'male'},
+//     {name:'Oksana',gender:'female'},
+//     {name:'Yulia',gender:'female'}
+//
+// ]
+
