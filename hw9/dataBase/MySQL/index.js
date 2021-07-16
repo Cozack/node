77c89cd6cd39941c46db160b4ff75157c5929937
Cozack <dudycholeg@gmail.com>
2021-07-16 18:38:09 +0300

    hw9 updated final

--- hw9/dataBase/MySQL/index.js
+++ hw9/dataBase/MySQL/index.js
@@ -9,8 +9,11 @@
 //
 // module.exports = connection.promise();
 const { Sequelize } = require('sequelize');
+const {
+    DB_USER_NAME, DB_USER_PASSWORD, DB_MYSQL_CONNECTION_NAME, DB_MYSQL_DIALECT
+} = require('../../constants/configuration');
 
-module.exports.sequelize = new Sequelize('test_sync', 'root', 'Martyska_08', {
-    dialect: 'mysql',
+module.exports.sequelize = new Sequelize(DB_MYSQL_CONNECTION_NAME, DB_USER_NAME, DB_USER_PASSWORD, {
+    dialect: DB_MYSQL_DIALECT,
     logging: false
 });
--- hw9/routes/mysql-router.js
+++ hw9/routes/mysql-router.js
@@ -5,7 +5,7 @@ const { mysqlController } = require('../controllers');
 router.get('/', mysqlController.findAllUser);
 router.post('/', mysqlController.create);
 router.get('/:id', mysqlController.findOne);
-router.put('/:id',mysqlController.updateUser);
+router.put('/:id', mysqlController.updateUser);
 router.delete('/:id', mysqlController.deleteUser);
 
 module.exports = router;
